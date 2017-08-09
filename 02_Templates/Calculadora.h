db7475151f2596a452ec4092f21b8e7574013535
Gabi Parrello <gabiparrello@gmail.com>
2017-08-09 10:30:05 -0300

    Se hizo el ejercicio de calculadora con Templates.

--- 02_Templates/Calculadora.h
+++ 02_Templates/Calculadora.h
@@ -2,7 +2,25 @@
 #define REPASO_CALCULADORA_H
 
 
+template<class T>
 class Calculadora {
+
+private:
+    T A;
+    T B;
+
+public:
+
+    T getA() const  { return A; }
+    T getB() const  { return B; }
+
+    void setA(T A)  { Calculadora::A = A; }
+    void setB(T B)  { Calculadora::B = B; }
+
+    T sumar()       { return A + B; }
+    T restar()      { return A - B; }
+    T multiplicar() { return A * B; }
+    T dividir()     { return A / B; }
 
 };
 
--- 02_Templates/main.cpp
+++ 02_Templates/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include "Calculadora.h"
+#include <string>
 
 using namespace std;
 
 int main() {
+
+    Calculadora<int> calcInt;
+    Calculadora<char> calcChar;
+    Calculadora<float> calcFloat;
+    Calculadora<string> calcString;
+
     cout << "Ejercicio 00/02\n" << endl;
+
+    //CALCULADORA DE INT
+    calcInt.setA(5);
+    calcInt.setB(2);
+
+    cout << "CALCULADORA DE INT" << endl << endl;
+    cout << "El resultado de 5 + 2 es: " << calcInt.sumar() << endl;
+    cout << "El resultado de 5 - 2 es: " << calcInt.restar() << endl;
+    cout << "El resultado de 5 * 2 es: " << calcInt.multiplicar() << endl;
+    cout << "El resultado de 5 / 2 es: " << calcInt.dividir() << endl << endl;
+
+
+    //CALCULADORA DE FLOAT
+    calcFloat.setA(3.7);
+    calcFloat.setB(2.5);
+
+    cout << "CALCULADORA DE FLOAT" << endl << endl;
+    cout << "El resultado de 3.7 + 2.5 es: " << calcFloat.sumar() << endl;
+    cout << "El resultado de 3.7 - 2.5 es: " << calcFloat.restar() << endl;
+    cout << "El resultado de 3.7 * 2.5 es: " << calcFloat.multiplicar() << endl;
+    cout << "El resultado de 3.7 / 2.5 es: " << calcFloat.dividir() << endl << endl;
+
+    //CALCULADORA DE STRING
+    calcString.setA("Hola, ");
+    calcString.setB("Mundo!");
+
+    cout << "SUMADOR DE STRING" << endl << endl;
+    cout << calcString.sumar() << endl;
+
     return 0;
 }
