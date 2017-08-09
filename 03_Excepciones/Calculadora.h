61a7cff892c944555fd459bb33d8dacf969fe5e1
Gabi Parrello <gabiparrello@gmail.com>
2017-08-09 11:17:01 -0300

    Se hizo el ejercicio de calculadora con Excepciones.

--- 03_Excepciones/Calculadora.h
+++ 03_Excepciones/Calculadora.h
@@ -2,7 +2,28 @@
 #define REPASO_CALCULADORA_H
 
 
+template<class T>
 class Calculadora {
+
+private:
+    T A;
+    T B;
+
+public:
+
+    T getA() const  { return A; }
+    T getB() const  { return B; }
+
+    void setA(T A)  { Calculadora::A = A; }
+    void setB(T B)  { Calculadora::B = B; }
+
+    T sumar()       { return A + B; }
+    T restar()      { return A - B; }
+    T multiplicar() { return A * B; }
+    T dividir() {
+        if (B == 0) throw 420;
+        return A / B;
+    }
 
 };
 
--- 03_Excepciones/main.cpp
+++ 03_Excepciones/main.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 
 int main() {
+
+    Calculadora<int> calc;
+
     cout << "Ejercicio 00/03\n" << endl;
+
+    //CALCULADORA DE INT
+    calc.setA(5);
+    calc.setB(0);
+
+    try {
+        int division = calc.dividir();
+        cout << "El resultado de 5 / 0 es: " << division << endl << endl;
+    }
+    catch (int e) {
+        cout << "B no puede ser 0. Excepcion N: " << e << endl;
+    }
+
     return 0;
 }
